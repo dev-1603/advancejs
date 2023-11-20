b95e693848bc91cecb60dd15da49ffa2612346b9
Debjyoti Mohapatra <53967786+dev-1603@users.noreply.github.com>
2023-11-20 13:38:48 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,5 @@
+
+//  initialize array of fixed length  and fill it with some value 
+// `n` is the size you want to initialize your array
+// `0` is what the array will be filled with (can be any other value)   bgut it will fail in typescript
+Array(n).fill(0)
